0436c51a901edb88dfd446502ec8f5d8c6013802
Ryan Carniato <ryansolid@gmail.com>
2018-05-25 00:38:12 -0700

    first commit

---
+++ README.md
@@ -0,0 +1,20 @@
+# React Solid State
+
+This is a local state swap for React using [Solid.js](https://github.com/ryansolid/solid). Instead of worry about when your components should update you can use declarative data.
+
+    import solidState from 'react-solid-state'
+    import React, { Component }  from 'react'
+
+    // Normal React Component
+    class MyComponent extends Component
+      constructor() {
+        this.state = {recipient: 'John'}
+      }
+
+      render() {
+        return <div onClick={() => this.state.set({recipient: 'Jake'})}>Hello {this.state.recipient}</div>
+      }
+
+    export default solidState(MyComponent)
+
+This library also supports Stateless pure function components.
---
+++ src/index.js
@@ -0,0 +1,94 @@
+import { State, Sync } from 'solid-js'
+import { Component } from 'react'
+RENDER_SYNC = Symbol('render sync');
+
+function isStatelessComponent(Comp) {
+  return !(Comp.prototype && Comp.prototype.render) && !Component.isPrototypeOf(Comp);
+};
+
+function toStatefulComponent(StatelessComponent) {
+  class StatefulComponent extends Component {
+      render() {
+        return StatelessComponent.call(this, this.props, this.context);
+      }
+  };
+
+  StatefulComponent.displayName = StatelessComponent.displayName || StatelessComponent.name;
+
+  StatefulComponent.propTypes = StatelessComponent.propTypes;
+
+  StatefulComponent.defaultTypes = StatelessComponent.defaultTypes;
+
+  StatefulComponent.contextTypes = StatelessComponent.contextTypes;
+
+  return StatefulComponent;
+};
+
+export default function(ReactComponent) {
+  var ReactSolidState;
+  if (isStatelessComponent(ReactComponent)) {
+    ReactComponent = toStatefulComponent(ReactComponent);
+  }
+
+  class ReactSolidState extends ReactComponent {
+    constructor(props) {
+      super(props);
+      this.state = new State(this.state || {});
+    }
+
+    render() {
+      var result;
+      if (this[RENDER_SYNC]) {
+        return super.render();
+      }
+      result = null;
+      this[RENDER_SYNC] = new Sync(() => {
+        // first render
+        if (!this[RENDER_SYNC]) {
+          result = super.render();
+          return;
+        }
+        return super.forceUpdate();
+      });
+      return result;
+    }
+
+    shouldComponentUpdate(next_props) {
+      var i, key, keys, len, next_keys;
+      keys = Object.keys(this.props);
+      next_keys = Object.keys(next_props);
+      if (keys.length !== next_keys.length) {
+        return true;
+      }
+      for (i = 0, len = keys.length; i < len; i++) {
+        key = keys[i];
+        if (this.props[key] !== next_props[key]) {
+          return true;
+        }
+      }
+      // prevent react state from rerender
+      return false;
+    }
+
+    componentWillUnmount() {
+      this.state.dispose();
+      this.state = null;
+      this[RENDER_SYNC].dispose();
+      this[RENDER_SYNC] = null;
+      if (super.componentWillUnmount) {
+        return super.componentWillUnmount(...arguments);
+      }
+    }
+
+  };
+
+  ReactSolidState.displayName = ReactComponent.displayName || ReactComponent.name;
+
+  ReactSolidState.propTypes = ReactComponent.propTypes;
+
+  ReactSolidState.defaultTypes = ReactComponent.defaultTypes;
+
+  ReactSolidState.contextTypes = ReactComponent.contextTypes;
+
+  return ReactSolidState;
+};
